902304cc35d118718b96cc5ebb9d11384d56ca7b
MrValk <n.s.peters05@gmail.com>
2023-02-01 21:19:03 +0100

    Fixed some issues with the GradeInfo component.

--- src/lib/components/cijfers/GradeInfo.svelte
+++ src/lib/components/cijfers/GradeInfo.svelte
@@ -2,52 +2,70 @@
 	import { selectedGrade } from '$stores/selectedGrade';
 
 	import { formatDate } from '$utils/formatDate';
+	import { onMount } from 'svelte';
 
 	let infoWidth: number = 0;
 	let infoEl: HTMLElement;
 
-	let observer = new ResizeObserver(() => {
-		if (infoEl) infoWidth = infoEl.offsetWidth;
+	let observer: ResizeObserver;
+
+	onMount(() => {
+		observer = new ResizeObserver(() => {
+			if (infoEl) infoWidth = infoEl.offsetWidth;
+		});
 	});
 
 	$: {
-		observer.disconnect();
-		if (infoEl) observer.observe(infoEl);
+		if (observer) {
+			observer.disconnect();
+			if (infoEl) observer.observe(infoEl);
+		}
 	}
 
-	function load(node: HTMLElement) {
+	function showInfo(node: HTMLElement) {
 		node.style.width = `${infoWidth}px`;
 		node.style.maxWidth = `${infoWidth}px`;
+
+		const rightCols = node.querySelectorAll('.right-col');
+		if (rightCols.length && infoWidth) {
+			// Convert rem to px, using the root font size
+			const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
+			rightCols.forEach((col) => {
+				const rightCol = col as HTMLElement;
+				rightCol.style.width = `${infoWidth - rem * 9}px`;
+				rightCol.style.maxWidth = `${infoWidth - rem * 9}px`;
+			});
+		}
 	}
 </script>
 
 <article class="text-lg h-full bg-zinc-500/70 rounded-md overflow-hidden shadow-xl">
 	<h3 class="flex items-center justify-center bg-zinc-700 font-bold h-10">Cijferinfo</h3>
 	{#if $selectedGrade}
-		<section use:load>
+		<section class="box-content" use:showInfo>
 			<div class="flex h-cell border-zinc-600/60 border-t">
 				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Naam</p>
-				<p class="flex items-center px-6">{$selectedGrade.name}</p>
+				<p class="right-col flex items-center px-6">{$selectedGrade.name}</p>
 			</div>
 			<div class="flex h-cell border-zinc-600/60 border-t">
 				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Vak</p>
-				<p class="flex items-center px-6">
+				<p class="right-col flex items-center px-6">
 					{$selectedGrade.subject.abbreviation} ({$selectedGrade.subject.name})
 				</p>
 			</div>
-			<div class="flex h-cell border-zinc-600/60 border-t">
-				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Beschrijving</p>
-				<p class="flex items-center px-6">
+			<div class="flex h-2cell border-zinc-600/60 border-t">
+				<p class="bg-zinc-600/50 h-2cell min-w-36 w-36 flex items-center pl-6">Beschrijving</p>
+				<p class="right-col flex items-center h-2cell px-6">
 					{$selectedGrade.description}
 				</p>
 			</div>
 			<div class="flex h-cell border-zinc-600/60 border-t">
 				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Type</p>
-				<p class="flex items-center px-6">{$selectedGrade.type}</p>
+				<p class="right-col flex items-center px-6">{$selectedGrade.type}</p>
 			</div>
 			<div class="flex h-cell border-zinc-600/60 border-t">
 				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Score</p>
-				<p class="flex items-center px-6">
+				<p class="right-col flex items-center px-6">
 					{$selectedGrade.score.toLocaleString(undefined, {
 						maximumFractionDigits: 1,
 						minimumFractionDigits: 1
@@ -55,18 +73,18 @@
 				</p>
 			</div>
 			<div class="flex h-cell border-zinc-600/60 border-t">
-				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Gewicht</p>
-				<p class="flex items-center px-6">{$selectedGrade.weight}</p>
+				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Weging</p>
+				<p class="right-col flex items-center px-6">{$selectedGrade.weight}</p>
 			</div>
 			{#if $selectedGrade.teacher.name}
 				<div class="flex h-cell border-zinc-600/60 border-t">
 					<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Docent</p>
-					<p class="flex items-center px-6">{$selectedGrade.teacher.name}</p>
+					<p class="right-col flex items-center px-6">{$selectedGrade.teacher.name}</p>
 				</div>
 			{/if}
 			<div class="flex h-cell border-zinc-600/60 border-t">
 				<p class="bg-zinc-600/50 min-w-36 w-36 flex items-center pl-6">Datum</p>
-				<p class="flex items-center px-6">{formatDate(new Date($selectedGrade.date))}</p>
+				<p class="right-col flex items-center px-6">{formatDate(new Date($selectedGrade.date))}</p>
 			</div>
 		</section>
 	{:else}
@@ -78,6 +96,10 @@
 
 <style lang="scss">
 	@use 'src/_vars.scss' as *;
+
+	.right-col {
+		word-break: break-all;
+	}
 
 	.min-w-36 {
 		min-width: 9rem;
